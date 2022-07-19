947563436642fa315479f99f36aeec7da155bf95
AYSI <aysanewyonasj@gmail.com>
2022-07-20 01:42:28 +0300

    Bubbling all the objects

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * bubble_sort - sorts an array in ascending order
+ * @array: array of ints to sort
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, j;
+	int tmp;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
